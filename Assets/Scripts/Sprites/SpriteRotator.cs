9fb91b12387f3b894690af980192aceaa0b0aa7d
misharuk05 <misharuk05@mail.ru>
2024-07-16 23:13:58 +0300

    Added enemy with animation

--- Assets/Scripts/Sprites/SpriteRotator.cs
+++ Assets/Scripts/Sprites/SpriteRotator.cs
@@ -7,6 +7,11 @@ public class SpriteRotator : MonoBehaviour
     [SerializeField] private Texture frontN, backN, leftN, rightN;
     [SerializeField] private SpriteRenderer _SR;
     [SerializeField] private Material _material;
+
+    private void Start()
+    {
+        _material.EnableKeyword("_NORMALMAP");
+    }
     void FixedUpdate()
     {
         transform.LookAt(player.position);
@@ -15,23 +20,28 @@ public class SpriteRotator : MonoBehaviour
         {
             if (angle <= 45f || angle > 315f)
             {
+                //Debug.Log(Shader.PropertyToID("_NORMALMAP"));
                 _SR.sprite = frontSprite;
-                _material.SetTexture(Shader.PropertyToID("_NORMALMAP"), frontN);
+                //_material.SetTexture(1327, frontN);
+                _material.SetTexture("_BumpMap", frontN);
             }
             else if (angle > 45f && angle <= 135f)
             {
                 _SR.sprite = leftSprite;
-                _material.SetTexture(Shader.PropertyToID("_NORMALMAP"), leftN);
+                //_material.SetTexture(Shader.PropertyToID("_NORMALMAP"), leftN);
+                _material.SetTexture("_BumpMap", leftN);
             }
             else if (angle > 135f && angle <= 225f)
             {
                 _SR.sprite = backSprite;
-                _material.SetTexture(Shader.PropertyToID("_NORMALMAP"), backN);
+                //_material.SetTexture(Shader.PropertyToID("_NORMALMAP"), backN);
+                _material.SetTexture("_BumpMap", backN);
             }
             else
             {
                 _SR.sprite = rightSprite;
-                _material.SetTexture(Shader.PropertyToID("_NORMALMAP"), rightN);
+                //_material.SetTexture(Shader.PropertyToID("_NORMALMAP"), rightN);
+                _material.SetTexture("_BumpMap", rightN);
             }
         }
     }
