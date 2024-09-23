5a6f6d30e2cde0b4f320f922a05f8281106fb236
D_One <misharuk05@mail.ru>
2024-09-23 18:08:39 +0300

    Added area damage for fireball

---
+++ Assets/Scripts/Projectiles/FireballProjectile.cs
@@ -0,0 +1,21 @@
+using Projectiles;
+using UnityEngine;
+
+public class FireballProjectile : Projectile
+{
+    [SerializeField] private float areaDamageRadius;
+    public void DamageArea()
+    {
+        Collider[] enemies = Physics.OverlapSphere(transform.position, transform.localScale.x * areaDamageRadius, targetLayer);
+        foreach (Collider enemy in enemies)
+        {
+            enemy.gameObject.GetComponent<EnemyHealth>().TakeDamage(damage / 2, damageType, isElementStrengthened);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, transform.localScale.x * areaDamageRadius);
+    }
+}
